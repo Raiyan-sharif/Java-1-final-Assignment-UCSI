16b2f1f41ccb8e87900817c5e78b7d0df6cbe117
Raiyan <Raiyan-sharif@users.noreply.github.com>
2017-03-19 23:23:36 +0600

    Add files via upload

--- Question13.java
+++ Question13.java
@@ -1,19 +1,33 @@
 import java.util.Scanner;
-class Question13{
+public class Question13{
+	
+	public static double sumMajorDiagonal(double m[][]){
+		double sum=0.0;
+		for(int i=0;i<4;i++){
+			for(int j=0;j<4;j++){
+				if(i==j){
+					sum = sum + m[i][j];
+				}
+			}
+		}
+		return sum;
+		
+	}
 	public static void main(String args[]){
-		int matrix[][] = new int[4][4];
-		int sum=0;
+		double matrix[][] = new double[4][4];
+		double sum=0;
 		Scanner sc= new Scanner(System.in); 
 		System.out.println("Enter a 4 by 4 matrix row by row:");
 		for(int i=0;i<4;i++){
 			for(int j=0;j<4;j++){
-				matrix[i][j] = sc.nextInt();
-				if(i==j){
-					sum+= matrix[i][j];
-				}
+				matrix[i][j] = sc.nextDouble();
+				//sumMajorDiagonal(matrix[i][j]);
+				
 			}
 		}
-		System.out.println("Sum of the elements in the major diagonal is "+(float)sum);
+		
+		sum = sumMajorDiagonal(matrix);
+		System.out.println("Sum of the elements in the major diagonal is "+sum);
 	}
 	
 }
